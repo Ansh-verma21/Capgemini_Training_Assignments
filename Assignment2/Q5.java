68db2c478f40998e4735ec4cf5285eb72efef49e
Ansh-verma21 <vermaansh13579@gmail.com>
2026-02-09 11:31:36 +0530

    Q5 done

---
+++ Assignment2/Q5.java
@@ -0,0 +1,24 @@
+public class SelectionSort {
+	public static void main(String[] args)
+	{
+		int arr[]= {1,3,5,7,9,2,4,6,8,0};
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}
